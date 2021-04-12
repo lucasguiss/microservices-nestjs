b97788c049b56f1856988fdff2e28494a864532e
Lucas Guiss <lucas.gusmao@roit.com.br>
2021-04-12 20:45:49 -0300

    feat(microservice-app): add app that will handle the messages

---
+++ microservices-app/dist/main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const microservices_1 = require("@nestjs/microservices");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.createMicroservice(app_module_1.AppModule, {
+        transport: microservices_1.Transport.TCP,
+    });
+    app.listen(() => console.log('Microservice is listening'));
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
